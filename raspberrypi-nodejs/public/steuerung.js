1a64b51bc6a8d863df8fb9a7cf809ef5f6a7a501
Ronny Hildebrandt <hilderonny2014@gmail.com>
2020-03-31 09:14:01 +0100

    Rückseite eingebaut.

---
+++ raspberrypi-nodejs/public/steuerung.js
@@ -0,0 +1,87 @@
+var Steuerung = (audioElement, leds) => {
+
+    function handleAudioLevel(level) {
+        leds.forEach((led) => {
+            socket.emit('led', { addr: led.addr, port: led.port, val: (level > led.audiolevel ? 1 : 0) });
+        });
+    }
+
+    function initAudio() {
+        // https://esonderegger.github.io/web-audio-peak-meter/
+        var audioCtx = new window.AudioContext();
+        var sourceNode = audioCtx.createMediaElementSource(audioElement);
+        sourceNode.connect(audioCtx.destination);
+        var meterNode = webAudioPeakMeter.createMeterNode(sourceNode, audioCtx);
+        var max = 0;
+        meterNode.onaudioprocess = function(audioProcessingEvent) {
+            var inputBuffer = audioProcessingEvent.inputBuffer;
+            var i;
+            var channelData = [];
+            var channelMaxes = [];
+            for (i = 0; i < meterNode.channelCount; i++) {
+            channelData[i] = inputBuffer.getChannelData(i);
+            channelMaxes[i] = 0.0;
+            }
+            for (var sample = 0; sample < inputBuffer.length; sample++) {
+                for (i = 0; i < meterNode.channelCount; i++) {
+                    var s = Math.abs(channelData[i][sample]);
+                    if (s > channelMaxes[i]) {
+                        channelMaxes[i] = s;
+                    }
+                    if (s > max) max = s;
+                }
+            }
+            //console.log(max, channelMaxes);
+            handleAudioLevel(channelMaxes[0]);
+        };
+        audioElement.addEventListener('play', function() {
+            audioCtx.resume();
+        });
+    }
+
+    var socket = io();
+
+    var onidx = 1;
+    var offidx = 0;
+    var llleds = leds.sort((a, b) => a.llidx - b.llidx);
+
+    function lauflicht() {
+        onidx += 1;
+        if (onidx >= llleds.length) {
+            onidx = 0;
+        }
+        var onled = llleds[onidx];
+        socket.emit('led', { addr: onled.addr, port: onled.port, val: 1 });
+        offidx += 1;
+        if (offidx >= llleds.length) {
+            offidx = 0;
+        }
+        var offled = llleds[offidx];
+        socket.emit('led', { addr: offled.addr, port: offled.port, val: 0 });
+    }
+
+    var ll;
+
+    initAudio();
+
+    return {
+        toggleLauflicht: () => {
+            if (ll) {
+                clearInterval(ll);
+                ll = null;
+            } else {
+                ll = setInterval(lauflicht, 100);
+            }
+        },
+        setleddirect: (index, on) => {
+            var led = leds[index];
+            socket.emit('led', { addr: led.addr, port: led.port, val: (on ? 1 : 0) });
+        },
+        toggleled: (index) => {
+            var led = leds[index];
+            led.on = !led.on;
+            socket.emit('led', { addr: led.addr, port: led.port, val: (led.on ? 1 : 0) });
+        },
+    }
+    
+};
